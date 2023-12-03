4d36ccee1131a851944f80a45160ec89fb617345
Dennis Karatas <thebr1dgeburner@proton.me>
2023-12-03 04:29:36 +0100

    FEAT: Type into search bar and receive the content

--- dist/index.js
+++ dist/index.js
@@ -40,12 +40,133 @@ const fs = __importStar(require("fs"));
 const commander_1 = require("commander");
 const figlet_1 = __importDefault(require("figlet"));
 const cheerio = __importStar(require("cheerio"));
+const puppeteer = __importStar(require("puppeteer"));
 const program = new commander_1.Command();
-console.log(figlet_1.default.textSync("ASHCROW'S ROM DOWNLOADER"));
+console.log(figlet_1.default.textSync("ASH'S ROM DOWNLOADER"));
 program
     .version("1.0.0")
-    .description("A simple download tool that allows quick search and download of ROMs for various emulators")
-    .option("-l, --ls [value]", "List directory contents");
+    .description("A simple tool that allows quick search and download of ROMs for various emulators")
+    .option("-c, --consoles [value]", "Lists all available consoles")
+    .option("-cs, --consolesearch <value>", "Search for a console")
+    .option("-cg, --cgames <value>", "Scroll through a console's library")
+    .option("-g, --game <value>", "Search for a game")
+    .parse(process.argv);
+const options = program.opts();
+//! Get all consoles to list
+if (options.consoles) {
+    const websiteUrl = "https://squid-proxy.xyz/";
+    getLinks(websiteUrl)
+        .then((links) => {
+        console.log("Found consoles:", links);
+    })
+        .catch((error) => {
+        console.error("Error fetching links:", error);
+    });
+}
+//! Search for a console
+if (options.consolesearch) {
+    const targetUrl = "https://squid-proxy.xyz/";
+    typeInSearchBar(targetUrl, options.consolesearch);
+}
+function typeInSearchBar(url, searchText) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const browser = yield puppeteer.launch({
+                headless: "new",
+            });
+            const page = yield browser.newPage();
+            yield page.goto(url);
+            // Fetch the HTML content using Puppeteer and pass it to Cheerio
+            const content = yield page.content();
+            const $ = cheerio.load(content);
+            const searchBar = $("#search");
+            // Perform actions with Puppeteer
+            if (searchBar.length > 0) {
+                yield page.type("#search", searchText);
+                const updatedContent = yield page.content();
+                const parsedContent = updatedContent.replace(/<tr[^>]*hidden[^>]*>.*?<\/tr>\n?/g, "");
+                const updated$ = cheerio.load(parsedContent);
+                const links = [];
+                const regex = /\//g;
+                const regexTwo = /\/|%20/g;
+                updated$("a").each((index, element) => {
+                    const href = $(element).attr("href");
+                    if (href) {
+                        if (href.slice(href.length - 1) == "/") {
+                            let parsedHref = href.replace(regex, "").replace(regexTwo, " ");
+                            if (!parsedHref.includes("http") &&
+                                !parsedHref.includes("NoIntro")) {
+                                console.log(parsedHref);
+                                links.push(parsedHref);
+                            }
+                        }
+                    }
+                });
+            }
+            else {
+                console.log("Search bar not found.");
+            }
+            // const updatedContent = await page.content();
+            // const updated$ = cheerio.load(updatedContent);
+            // const links = updated$("a").filter((index, element) => {
+            //   const parentRow = $(element).closest("tr");
+            //   return parentRow.css("display") !== "none";
+            // });
+            // console.log(links);
+            // const cleanedLinks: string[] = [];
+            // const regex = /\//g;
+            // const regexTwo = /\/|%20/g;
+            // links.each((index, element) => {
+            //   const href = $(element).attr("href");
+            //   if (href) {
+            //     if (href.slice(href.length - 1) == "/") {
+            //       let parsedHref = href.replace(regex, "").replace(regexTwo, " ");
+            //       if (!parsedHref.includes("http") && !parsedHref.includes("NoIntro")) {
+            //         console.log(parsedHref);
+            //         cleanedLinks.push(parsedHref);
+            //       }
+            //     }
+            //   }
+            // });
+            // links.each((index, element) => {
+            //   const href = updated$(element).attr("href");
+            //   console.log(href);
+            // });
+            // updated$("a").each((index, element) => {
+            //   const href = $(element).attr("href");
+            //   if (href) {
+            //     if (href.slice(href.length - 1) == "/") {
+            //       let parsedHref = href.replace(regex, "").replace(regexTwo, " ");
+            //       if (!parsedHref.includes("http") && !parsedHref.includes("NoIntro")) {
+            //         console.log(parsedHref);
+            //         links.push(parsedHref);
+            //       }
+            //     }
+            //   }
+            // });
+            yield browser.close();
+        }
+        catch (error) {
+            console.error("Error:", error);
+        }
+    });
+}
+// Usage example
+// const targetUrl = "https://squid-proxy.xyz/";
+// const searchTerm = "playstation";
+// typeInSearchBar(targetUrl, searchTerm);
+// const fileUrl =
+//   "https://squid-proxy.xyz/Nintendo%20DS/%27Chou%27%20Kowai%20Hanashi%20DS%20-%20Ao%20no%20Shou%20(Japan).zip";
+// const savePath = "./downloadedDir.zip";
+//! Example of how to download a file and save it in the same directory
+// downloadFile(fileUrl, savePath)
+//   .then(() => {
+//     console.log("File downloaded successfully!");
+//   })
+//   .catch((error) => {
+//     console.error("Error downloading file:", error);
+//   });
+//! Downloads a file
 function downloadFile(url, destinationPath) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -66,6 +187,7 @@ function downloadFile(url, destinationPath) {
         }
     });
 }
+//! Get all of a website's links
 function getLinks(url) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -93,22 +215,4 @@ function getLinks(url) {
         }
     });
 }
-const fileUrl = "https://squid-proxy.xyz/Nintendo%20DS/%27Chou%27%20Kowai%20Hanashi%20DS%20-%20Ao%20no%20Shou%20(Japan).zip";
-const savePath = "./downloadedDir.zip";
-//! Example of how to download a file and save it in the same directory
-// downloadFile(fileUrl, savePath)
-//   .then(() => {
-//     console.log("File downloaded successfully!");
-//   })
-//   .catch((error) => {
-//     console.error("Error downloading file:", error);
-//   });
-const websiteUrl = "https://squid-proxy.xyz/";
-getLinks(websiteUrl)
-    .then((links) => {
-    console.log("Found consoles:", links);
-})
-    .catch((error) => {
-    console.error("Error fetching links:", error);
-});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,110 @@ import * as fs from "fs";
 import { Command } from "commander";
 import figlet from "figlet";
 import * as cheerio from "cheerio";
+import * as puppeteer from "puppeteer";
 
 const program = new Command();
 
-console.log(figlet.textSync("ASHCROW'S ROM DOWNLOADER"));
+console.log(figlet.textSync("ASH'S ROM DOWNLOADER"));
 
 program
   .version("1.0.0")
   .description(
-    "A simple download tool that allows quick search and download of ROMs for various emulators"
+    "A simple tool that allows quick search and download of ROMs for various emulators"
   )
-  .option("-l, --ls [value]", "List directory contents");
+  .option("-c, --consoles [value]", "Lists all available consoles")
+  .option("-cs, --consolesearch <value>", "Search for a console")
+  .option("-cg, --cgames <value>", "Scroll through a console's library")
+  .option("-g, --game <value>", "Search for a game")
+  .parse(process.argv);
 
+const options = program.opts();
+
+//! Get all consoles to list
+if (options.consoles) {
+  const websiteUrl = "https://squid-proxy.xyz/";
+  getLinks(websiteUrl)
+    .then((links) => {
+      console.log("Found consoles:", links);
+    })
+    .catch((error) => {
+      console.error("Error fetching links:", error);
+    });
+}
+//! Search for a console
+if (options.consolesearch) {
+  const targetUrl = "https://squid-proxy.xyz/";
+
+  typeInSearchBar(targetUrl, options.consolesearch);
+}
+
+//! Scroll through a console's library
+
+async function typeInSearchBar(url: string, searchText: string) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const content = await page.content();
+    const $ = cheerio.load(content);
+
+    const searchBar = $("#search");
+
+    // Perform actions with Puppeteer
+    if (searchBar.length > 0) {
+      await page.type("#search", searchText);
+      const updatedContent = await page.content();
+      const parsedContent = updatedContent.replace(
+        /<tr[^>]*hidden[^>]*>.*?<\/tr>\n?/g,
+        ""
+      );
+      const updated$ = cheerio.load(parsedContent);
+
+      const links: string[] = [];
+      const regex = /\//g;
+      const regexTwo = /\/|%20/g;
+
+      updated$("a").each((index, element) => {
+        const href = $(element).attr("href");
+        if (href) {
+          if (href.slice(href.length - 1) == "/") {
+            let parsedHref = href.replace(regex, "").replace(regexTwo, " ");
+            if (
+              !parsedHref.includes("http") &&
+              !parsedHref.includes("NoIntro")
+            ) {
+              console.log(parsedHref);
+              links.push(parsedHref);
+            }
+          }
+        }
+      });
+    } else {
+      console.log("Search bar not found.");
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+//! Example of how to download a file and save it in the same directory
+// const fileUrl =
+//   "https://squid-proxy.xyz/Nintendo%20DS/%27Chou%27%20Kowai%20Hanashi%20DS%20-%20Ao%20no%20Shou%20(Japan).zip";
+// const savePath = "./downloadedDir.zip";
+// downloadFile(fileUrl, savePath)
+//   .then(() => {
+//     console.log("File downloaded successfully!");
+//   })
+//   .catch((error) => {
+//     console.error("Error downloading file:", error);
+//   });
+
+//! Downloads a file
 async function downloadFile(
   url: string,
   destinationPath: string
@@ -38,6 +130,7 @@ async function downloadFile(
   }
 }
 
+//! Get all of a website's links
 async function getLinks(url: string): Promise<string[]> {
   try {
     const response = await axios.get(url);
@@ -64,26 +157,3 @@ async function getLinks(url: string): Promise<string[]> {
     throw new Error("Error fetching links");
   }
 }
-
-const fileUrl =
-  "https://squid-proxy.xyz/Nintendo%20DS/%27Chou%27%20Kowai%20Hanashi%20DS%20-%20Ao%20no%20Shou%20(Japan).zip";
-const savePath = "./downloadedDir.zip";
-
-//! Example of how to download a file and save it in the same directory
-// downloadFile(fileUrl, savePath)
-//   .then(() => {
-//     console.log("File downloaded successfully!");
-//   })
-//   .catch((error) => {
-//     console.error("Error downloading file:", error);
-//   });
-
-const websiteUrl = "https://squid-proxy.xyz/";
-
-getLinks(websiteUrl)
-  .then((links) => {
-    console.log("Found consoles:", links);
-  })
-  .catch((error) => {
-    console.error("Error fetching links:", error);
-  });
